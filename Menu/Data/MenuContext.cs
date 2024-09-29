02dcca4ac89b1cff05ee741a8b269d739bddf5f9
Rohit Mahuli <128014013+rohitmuta@users.noreply.github.com>
2024-09-28 21:19:37 -0500

    Add files via upload

---
+++ Menu/Data/MenuContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+
+namespace Menu.Data
+{
+    public class MenuContext: DbContext
+    {
+        public MenuContext( DbContextOptions<MenuContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new
+            {
+                di.DishId,
+                di.IngredientId
+
+            });
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish { Id = 1, Name = "Margarita Pizza", Price = 7.99, 
+                    ImageUrl = "https://cdn.loveandlemons.com/wp-content/uploads/2023/07/margherita-pizza-recipe.jpg" });
+            modelBuilder.Entity<Ingredients>().HasData(
+                new Ingredients { Id = 1, Name = " Tomato Sauce" },
+                new Ingredients { Id = 2, Name = " Mozzarella" }
+                );
+
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient { DishId = 1, IngredientId = 1 },
+                new DishIngredient { DishId = 1, IngredientId = 2 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+                
+        }
+
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Ingredients> Ingredients { get; set; }
+        public DbSet<DishIngredient> DishIngredients { get; set; }
+    }
+}
